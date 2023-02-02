21f1d09cd242662da8b040bbbffbee4cd1485658
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-02-02 17:46:54 +0530

    Update ec2-wordpress.tf

--- remote-exec-example/ec2-wordpress.tf
+++ remote-exec-example/ec2-wordpress.tf
@@ -1,42 +1,48 @@
 resource "aws_instance" "webserver" {
+depends_on = [
+    aws_security_group.websg
+  ]
 
-   
+
   # AMI ID [I have used my custom AMI which has some softwares pre installed]
-  ami = "ami-00874d747dde814fa"
+  ami           = "ami-00874d747dde814fa"
   instance_type = "t2.micro"
-  subnet_id = subnet-062ac4058a80adee1
+  subnet_id     = "subnet-062ac4058a80adee1"
+  associate_public_ip_address = "true"
 
   # Keyname and security group are obtained from the reference of their instances created above!
   # Here I am providing the name of the key which is already uploaded on the AWS console.
   key_name = "samim"
-  
+
   # Security groups to use!
-  vpc_security_group_ids = vpc-00817a61882d246a5
+  vpc_security_group_ids =  ["${aws_security_group.websg.id}"]
 
   tags = {
-   Name = "Webserver_From_Terraform"
+    Name = "Webserver_From_Terraform"
   }
 
   # Installing required softwares into the system!
   connection {
-    type = "ssh"
-    user = "ubuntu"
-    private_key = file("samim.pem")
-    host = aws_instance.webserver.public_ip
-  }
-
+      host        = self.public_ip
+      type        = "ssh"
+      port        = 22
+      user        = "ubuntu"
+      private_key = file("samim.pem")
+      timeout     = "1m"
+      agent       = false
+    }
   # Code for installing the softwares!
   provisioner "remote-exec" {
     inline = [
-        "sudo yum update -y",
-        "sudo yum install php php-mysqlnd httpd -y",
-        "wget https://wordpress.org/wordpress-4.8.14.tar.gz",
-        "tar -xzf wordpress-4.8.14.tar.gz",
-        "sudo cp -r wordpress /var/www/html/",
-        "sudo chown -R apache.apache /var/www/html/",
-        "sudo systemctl start httpd",
-        "sudo systemctl enable httpd",
-        "sudo systemctl restart httpd"
+      "sudo yum update -y",
+      "sudo yum install php php-mysqlnd httpd -y",
+      "wget https://wordpress.org/wordpress-4.8.14.tar.gz",
+      "tar -xzf wordpress-4.8.14.tar.gz",
+      "sudo cp -r wordpress /var/www/html/",
+      "sudo chown -R apache.apache /var/www/html/",
+      "sudo systemctl start httpd",
+      "sudo systemctl enable httpd",
+      "sudo systemctl restart httpd"
     ]
   }
 }
