67a845fde45e695411821954c4d25e58cf10072e
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-02-02 17:45:48 +0530

    Create websg.tf

---
+++ remote-exec-example/websg.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "websg" {
+  name   = "HTTP and SSH"
+  vpc_id = "vpc-00817a61882d246a5"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
