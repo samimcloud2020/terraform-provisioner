c5d66da30de7ae949f6771a6718a95db57777a92
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-02-03 21:48:10 +0530

    Create websg.tf

---
+++ file-provisioner/websg.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "websg" {
+  name   = "HTTP and SSH and 8080"
+  vpc_id = "vpc-00817a61882d246a5"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
