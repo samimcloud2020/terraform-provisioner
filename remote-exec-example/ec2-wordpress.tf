064858915fe0e7a2ebc0ffa5bb978b96b79f40a9
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-04-18 13:02:55 +0530

    Update ec2-wordpress.tf

--- remote-exec-example/ec2-wordpress.tf
+++ remote-exec-example/ec2-wordpress.tf
@@ -2,26 +2,16 @@ resource "aws_instance" "webserver" {
 depends_on = [
     aws_security_group.websg
   ]
-
-
-  # AMI ID [I have used my custom AMI which has some softwares pre installed]
   ami           = "ami-00874d747dde814fa"
   instance_type = "t2.micro"
   subnet_id     = "subnet-062ac4058a80adee1"
   associate_public_ip_address = "true"
-
-  # Keyname and security group are obtained from the reference of their instances created above!
-  # Here I am providing the name of the key which is already uploaded on the AWS console.
   key_name = "samim"
-
-  # Security groups to use!
   security_groups =  [ "aws_security_group.websg.id" ]
-
   tags = {
     Name = "Webserver_From_Terraform"
   }
 
-  # Installing required softwares into the system!
   connection {
       host        = self.public_ip
       type        = "ssh"
@@ -31,7 +21,7 @@ depends_on = [
       timeout     = "1m"
       agent       = false
     }
-  # Code for installing the softwares!
+  
   provisioner "remote-exec" {
     inline = [
        resource "aws_instance" "webserver" {
@@ -39,25 +29,16 @@ depends_on = [
     aws_security_group.websg
   ]
 
-
-  # AMI ID [I have used my custom AMI which has some softwares pre installed]
   ami                         = "ami-00874d747dde814fa"
   instance_type               = "t2.micro"
   subnet_id                   = "subnet-062ac4058a80adee1"
   associate_public_ip_address = "true"
-
-  # Keyname and security group are obtained from the reference of their instances created above!
-  # Here I am providing the name of the key which is already uploaded on the AWS console.
   key_name = "samim"
-
-  # Security groups to use!
   security_groups = ["${aws_security_group.websg.id}"]
-
   tags = {
     Name = "Webserver_From_Terraform"
   }
 
-  # Installing required softwares into the system!
   connection {
     host        = self.public_ip
     type        = "ssh"
@@ -66,8 +47,7 @@ depends_on = [
     private_key = file("samim.pem")
     timeout     = "1m"
     agent       = false
-  }
-  # Code for installing the softwares!
+ 
   provisioner "remote-exec" {
     inline = [
       "sudo apt update -y",
