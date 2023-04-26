b4b7841bbfcb831fba7f55f853cee2610ed310d0
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-04-26 17:08:25 +0530

    Create var.tf

---
+++ for_each/var.tf
@@ -0,0 +1,17 @@
+/* A list variable holds a list of values */
+
+variable instance_type1 {
+    type = list
+    default = [ "t2.micro", "t3.micro" ]
+}
+
+/*A map value is a lookup table from string keys to string values.
+ This is useful for selecting a value based on some other provided value.*/
+
+variable instance_type2 {
+    type = map(string)
+    default = {
+    "us-east-1" = "t2.micro"
+    "us-west-2" = "t3.micro"
+  }
+}
