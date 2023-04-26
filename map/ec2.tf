d24c950fc19d74ec5747ea8fd2e97ffb14f807a7
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-04-26 17:24:51 +0530

    Create ec2.tf

---
+++ map/ec2.tf
@@ -0,0 +1,30 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_instance" "web1" {
+  
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type2["us-east-1"]
+  availability_zone = var.az["us-east-1"] 
+  provisioner "local-exec" {
+   command = "echo ${self.private_ip} >> private_ip1.txt"
+ }
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
