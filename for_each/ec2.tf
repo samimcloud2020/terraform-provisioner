60a0dc397a70bbb017f9b6c84caa24137e092cb8
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-04-26 17:07:31 +0530

    Create ec2.tf

---
+++ for_each/ec2.tf
@@ -0,0 +1,31 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_instance" "web1" {
+  
+  ami           = data.aws_ami.ubuntu.id
+  for_each = toset(var.instance_type1)
+  instance_type = each.key
+
+  provisioner "local-exec" {
+   command = "echo ${self.private_ip} >> private_ip1.txt"
+ }
+  tags = {
+    Name = "HelloWorld ${each.key}"
+  }
+}
+
