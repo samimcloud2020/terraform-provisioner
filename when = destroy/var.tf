f8d3815806bfba423f121fc8b33560e7ce16ccd3
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-04-27 13:46:55 +0530

    Create var.tf

---
+++ when = destroy/var.tf
@@ -0,0 +1,30 @@
+/* A list variable holds a list of values */
+
+variable instance_type1 {
+    type = list
+    default = [ "t2.micro", "t3.micro" ]
+}
+
+/*A map value is a lookup table from string keys to string values.
+ This is useful for selecting a value based on some other provided value.*/
+
+variable instance_type2 {
+    type = map(string)
+    default = {
+    "us-east-1" = "t2.micro"
+    "us-west-2" = "t3.micro"
+  }
+}
+
+variable az {
+    type = map(string)
+    default = {
+    "us-east-1" = "us-east-1a"
+    "us-west-2" = "us-west-2a"
+  }
+}
+
+variable region {
+  type =string
+  default = "us-east-1"
+}
