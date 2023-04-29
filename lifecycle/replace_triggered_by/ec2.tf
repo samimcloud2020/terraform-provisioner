20f9c530c8e0f1da0136070c4b5bf8f2d3235d4d
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-04-29 16:30:50 +0530

    Update ec2.tf

--- lifecycle/replace_triggered_by/ec2.tf
+++ lifecycle/replace_triggered_by/ec2.tf
@@ -1,110 +1,62 @@
 # Create a VPC
-resource "aws_vpc" "app_vpc" {
-  cidr_block = "192.168.0.0/16"
-
-  tags = {
-    Name = "app-vpc"
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.app_vpc.id
-
-  tags = {
-    Name = "vpc_igw"
-  }
-}
-
-resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.app_vpc.id
-  cidr_block        = "192.168.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "public-subnet"
-  }
-}
-
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.app_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "public_rt"
-  }
-}
-
-resource "aws_route_table_association" "public_rt_asso" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
-}
 
 resource "aws_instance" "web1" {
   ami             = "ami-007855ac798b5175e" 
   instance_type   = "t2.micro"
   key_name        = "bsnl"
-  subnet_id       = aws_subnet.public_subnet.id
-  security_groups = [aws_security_group.sg.id]
-   
+  subnet_id       = "subnet-062ac4058a80adee1"
+  security_groups = ["sg-040632956d3752c46"]
+  
   user_data = <<-EOF
   #!/bin/bash
   echo "*** Installing apache2"
   sudo apt update -y
   sudo apt install apache2 -y
-  echo "<h1>welcome apache2  <\h1>" > /var/www/html/index.html
+  echo "<h1>welcome apache2 web1  <\h1>" > /var/www/html/index.html
   sudo systemctl restart apache2
   echo "*** Completed Installing apache"
   EOF
 
   tags = {
-    Name = "web_instance"
+    Name = "web_instance1"
   }
 
   volume_tags = {
-    Name = "web_instance"
+    Name = "web_instance1"
   } 
 }
 
 
-resource "aws_security_group" "sg" {
-  name        = "allow_ssh_http"
-  description = "Allow ssh http inbound traffic"
-  vpc_id      = aws_vpc.app_vpc.id
 
-  ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
 
-  ingress {
-    description      = "HTTP from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+resource "aws_instance" "web2" {
+  ami             = "ami-007855ac798b5175e" 
+  instance_type   = "t2.micro"
+  key_name        = "bsnl"
+  subnet_id       = "subnet-062ac4058a80adee1"
+  security_groups = ["sg-040632956d3752c46"]
+  lifecycle {
+    replace_triggered_by = [
+      aws_instance.web1.user_data
+    ]
+  }  
+  user_data = <<-EOF
+  #!/bin/bash
+  echo "*** Installing apache2"
+  sudo apt update -y
+  sudo apt install apache2 -y
+  echo "<h1>welcome apache2 web2<\h1>" > /var/www/html/index.html
+  sudo systemctl restart apache2
+  echo "*** Completed Installing apache"
+  EOF
 
   tags = {
-    Name = "allow_ssh_http"
+    Name = "web_instance2"
   }
+
+  volume_tags = {
+    Name = "web_instance2"
+  } 
 }
 
 
